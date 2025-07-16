38459799d05b5c1b8fdf42195a38bd360615799d
Atharva <chitreatharva11@gmail.com>
2025-07-16 11:00:00 +0530

    Add route to display sweets and test for route

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from codes.models import db, Sweet
-from codes.routes import create_routes, read_routes, update_routes
+from codes.routes import create_routes, read_routes, update_routes, delete_routes
+from codes.services import SAMPLE_SWEETS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sweets.db'
@@ -11,8 +12,19 @@ db.init_app(app)
 app.register_blueprint(create_routes)
 app.register_blueprint(read_routes)
 app.register_blueprint(update_routes)
+app.register_blueprint(delete_routes)
+
+@app.cli.command("init-db")
+def init_db():
+    with app.app_context():
+        db.create_all()
+        for s in SAMPLE_SWEETS:
+            if not Sweet.query.filter_by(name=s['name']).first():
+                db.session.add(Sweet(name=s['name'], price=s['price'], quantity=10))
+        db.session.commit()
+    print("Database initialized!")
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True) 
